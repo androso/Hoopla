f93c8274080225031916562049b623ba89f1809c
anibal andrade <59257555+androso@users.noreply.github.com>
2025-11-01 19:26:53 +0000

    adding basic search

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,5 +1,22 @@
 import argparse
+import json
+from typing import Any, Dict, List
 
+MOVIES_FILE = "data/movies.json"
+MAX_RESULTS = 5
+
+def load_movies(filepath: str) -> List[Dict[str, Any]]:
+    with open(filepath, "r") as f:
+        data = json.load(f)
+        return data["movies"]
+
+def search_movie(moviesList: List[Dict[str, Any]], query: str) -> List[Dict[str, Any]]:
+    result = []
+    for movie in moviesList:
+       if query in movie["title"]:
+          result.append(movie) 
+    return result
+    
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
@@ -8,10 +25,16 @@ def main() -> None:
     search_parser.add_argument("query", type=str, help="Search query")
 
     args = parser.parse_args()
+    
     match args.command:
         case "search":
-            print(args.query)
-            pass
+            print("Searching for:", args.query)
+            movies = load_movies("data/movies.json")
+            results = search_movie(movies, args.query) 
+            for index, movie in enumerate(results):
+                if (index > 4):
+                    break
+                print(f"{index + 1}.", movie["title"])
         case _:
             parser.print_help()
             
