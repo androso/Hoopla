fe976541f3b2fa2d87ab39f2fbb0bd2cb5e30ba9
androso <androso421@gmail.com>
2026-02-15 18:09:52 -0600

    Add F1 Score calculation to evaluation CLI output

--- cli/evaluation_cli.py
+++ cli/evaluation_cli.py
@@ -24,18 +24,18 @@ def main():
         relevant_docs = test_case["relevant_docs"]
         results = instance_search.rrf_search(query, 60, limit)
         relevant_docs_found_n = 0
-        # relevant_retrieved_docs = []
         for result in results:
             if result["title"] in relevant_docs:    
                 relevant_docs_found_n += 1
-                # relevant_retrieved_docs.append(result["title"])
         precision = relevant_docs_found_n / len(results)
         recall = relevant_docs_found_n / len(relevant_docs)
+        f1 = 2 * (precision * recall) / (precision + recall)
 
         print(f"k={limit}")
         print(f"- {query}")
         print(f"    - Precision@{limit}: {precision:.4f}")
         print(f"    - Recall@{limit}: {recall:.4f}")
+        print(f"    - F1 Score: {f1}")
         print(f"    - Retrieved: {[result['title'] for result in results]}")
         print(f"    - Relevant: {[doc for doc in relevant_docs]}")
 
